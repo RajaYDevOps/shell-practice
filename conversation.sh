26cd122c4615a520948d32504a2c0884ac8a22b5
RajaYDevOps <crr.yannam90@gmail.com>
2026-06-13 11:17:59 +0530

    hello-world

---
+++ conversation.sh
@@ -0,0 +1,9 @@
+!#/bin/bash
+
+PERSON1=Trump
+PERSON2=IRAN
+
+echo "$PERSON1: Hey Iran ,we are going to blast you!"
+echo "$PERSON2: Yes,please.You are welcom"
+echo "$PERSON1: Hey $PERSON2, i am serious"
+echo "$PERSON1: Hey $PERSON1, we are also serious"
