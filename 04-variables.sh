0254ecfafc83cf1f9610544634b396369dee39d9
RajaYDevOps <crr.yannam90@gmail.com>
2026-06-13 12:32:06 +0530

    hello-world

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,9 +3,9 @@
 echo "Please enter your username:"
 read USER_NAME # here username is a variable.
 
-echo "Username is:$USER_NAME"
+echo "Username is: $USER_NAME"
 
 echo "Please enter your password"
 read -s Password
 
-echo "Password is:$Password"
+echo "Password is: $Password"
