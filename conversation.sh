4947e908804ed663594065f9faea3577a5dc703b
RajaYDevOps <crr.yannam90@gmail.com>
2026-06-13 11:28:43 +0530

    hello-world

--- conversation.sh
+++ conversation.sh
@@ -6,4 +6,4 @@ PERSON2=Iran
 echo "$PERSON1: Hey $PERSON2 ,we are going to blast you!"
 echo "$PERSON2: Yes,please.You are welcome"
 echo "$PERSON1: Hey $PERSON2, i am serious"
-echo "$PERSON1: Hey $PERSON1, we are also serious"
+echo "$PERSON2: Hey $PERSON1, we are also serious"
