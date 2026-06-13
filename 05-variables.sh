3dee766a802dcd3f45884569ca6f870f51cf2597
RajaYDevOps <crr.yannam90@gmail.com>
2026-06-13 12:38:41 +0530

    hello-world

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,4 +1,14 @@
 !#/bin/bash
 
-TIMESTAMP=$(date)
-echo "Time is $TIMESTAMP"
+# TIMESTAMP=$(date)
+# echo "Time is $TIMESTAMP"
+
+start_time=$(date + %s)
+
+sleep 10
+
+end_time=$(date + %s)
+
+total_time=$($end_time-$start_time)
+
+echo "total time taken is $total_time seconds"
