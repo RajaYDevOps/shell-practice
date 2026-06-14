58b0653c208ff4a6478f501ce0ca51a796c5cfda
RajaYDevOps <crr.yannam90@gmail.com>
2026-06-14 17:51:36 +0530

    updating special-vars

--- data-types.sh
+++ data-types.sh
@@ -10,3 +10,6 @@ echo "Sum is: $SUM"
 #Array
 MOVIES=(RRR Varanasi Peddi) #index always start from  0
 echo "Movies are: ${MOVIES[@]}"
+echo "First Movie is: ${MOVIES[0]}"
+echo "Second Movie is: ${MOVIES[1]}"
+echo "Third Movie is: ${Movies[2]}"
