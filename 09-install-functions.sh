584abe414717138899a970749420200326605dfb
RajaYDevOps <crr.yannam90@gmail.com>
2026-06-14 22:59:38 +0530

    installing functions

--- 09-install-functions.sh
+++ 09-install-functions.sh
@@ -8,21 +8,34 @@ if [ $USER_ID -ne 0 ]; then
   exit 1
 fi
 
+# first arg -->what are you trying to install
+# second arg-->exit code
+VALIDATE(){
+  if [ $2 -ne 0 ]; then
+   echo "installing $1 is....FAILED"
+   exit 1
+  else
+   echo "installing $1 is....SUCCESS"
+fi
+}
 #echo "I am continuing...."
 dnf list installed mysql
-
 if [ $? -eq 0 ]; then
- echo "already installed.....Skipping"
+  echo "already installed.....Skipping"
 else 
-echo "Installing mySQL"
-dnf install mysql -y
-if [ $? -ne 0 ]; then
- echo "installing mysql is....FAILED"
- exit 1
-else
- echo "installing mysql is....SUCCESS"
+  echo "Installing mySQL"
+  dnf install mysql -y
+  VALIDATE MySQL $?
 fi
+dnf list installed nginx
+if [ $? -eq 0 ]; then
+  echo "already installed.....Skipping"
+else 
+  echo "Installing nginx"
+  dnf install nginx -y
+  VALIDATE nginx $?
 fi
+
 
 # echo "I am continuing...."
 # echo "I am still continuing"
