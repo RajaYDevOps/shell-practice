0e7c6a161b34ed7fdf909b26432fcac4ce74901c
RajaYDevOps <crr.yannam90@gmail.com>
2026-06-14 20:08:44 +0530

    updating special-vars

--- conditions.sh
+++ conditions.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-NUMBER=$1
+# NUMBER=$1
 
-if [ $NUMBER -gt 20 ]; then
+# if [ $NUMBER -gt 20 ]; then
+#     echo "given number $NUMBER is greater than 20"
+# elif [ $NUMBER -eq 20 ]; then
+#     echo "given number $NUMBER is equal to 20"
+# else
+#      echo "given number $NUMBER is less than 20"
+# fi
+
+
+NUMBER1=$1
+NUMBER2=$2
+
+if [ $NUMBER1 -gt $NUMBER ]; then
     echo "given number $NUMBER is greater than 20"
-elif [ $NUMBER -eq 20 ]; then
+elif [ $NUMBER1 -eq NUMBER2 ]; then
     echo "given number $NUMBER is equal to 20"
 else
      echo "given number $NUMBER is less than 20"
