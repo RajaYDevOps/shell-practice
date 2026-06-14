c4c4ac980340372791ede0588837c2e60b44c8a9
RajaYDevOps <crr.yannam90@gmail.com>
2026-06-14 15:29:56 +0530

    updating special-vars

--- conversation.sh
+++ conversation.sh
@@ -1,9 +1,19 @@
 !#/bin/bash
 
-PERSON1=China
-PERSON2=Taiwan
+# PERSON1=China
+# PERSON2=Taiwan
 
-echo "$PERSON1: Hey $PERSON2 ,we are going to blast you!"
-echo "$PERSON2: Yes,please.You are welcome"
-echo "$PERSON1: Hey $PERSON2, i am serious"
-echo "$PERSON2: Hey $PERSON1, we are also serious"
+# echo "$PERSON1: Hey $PERSON2 ,we are going to blast you!"
+# echo "$PERSON2: Yes,please.You are welcome"
+# echo "$PERSON1: Hey $PERSON2, i am serious"
+# echo "$PERSON2: Hey $PERSON1, we are also serious"
+
+#to check all variables,count of variables passed
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$1: Hey $2 ,we are going to blast you!"
+echo "$2: Yes,please.You are welcome"
+echo "$1: Hey $PERSON2, i am serious"
+echo "$2: Hey $PERSON1, we are also serious"
