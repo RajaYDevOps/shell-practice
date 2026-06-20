29e1bcb384dd6f6c0d0b93e3d3c9a254f2f5c255
RajaYDevOps <crr.yannam90@gmail.com>
2026-06-20 17:07:43 +0530

    validations

--- 12-loops.sh
+++ 12-loops.sh
@@ -25,7 +25,7 @@ for package in $@
 do
   echo "Installing $package"
   dnf list installed $package &>> $LOGS_FILE
-   if [ $? -eq 0]; then
+   if [ $? -eq 0 ]; then
     echo "MySQL already installed.....Skipping"
    else
    #echo "Installing $package"
